df2d5e459a4988c05bc951c7a3a67331801b67c7
ksukhova <bonafide.kate@gmail.com>
2017-07-13 18:05:02 +0300

    footer cleanup

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,30 +2,11 @@ import $ from 'jquery';
 import './footer.scss';
 
 export default class Footer {
-    static selectors = {
-        button: '.button'
-    };
-
     constructor($root) {
 
         this.elements = {
             $root,
             $window: $(window)
         };
-
-        this.data = null;
-        this.init();
-    }
-
-    init() {
-
-    }
-
-    attachEvents() {
-
-    }
-
-    handleClick() {
-
     }
 }
